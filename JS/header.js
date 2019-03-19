ae4de58cc10d4bb0bb4a14b49123d61935ae6a0b
Sanchez Serna <gjsanchez2@mail.mccneb.edu>
2019-03-18 19:08:15 -0500

    website

---
+++ JS/header.js
@@ -0,0 +1,27 @@
+var myVar;
+
+function spinWheel() {
+  myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+  document.getElementById("form").style.display = "black";
+}
+
+// new code 
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    };
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+}
