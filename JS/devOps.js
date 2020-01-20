93540dec882f66cae3b6e77e2c20964468518fd4
Gabriel Sanchez <gabrielsanchezserna@outlook.com>
2020-01-20 09:10:00 -0600

    week-3

--- JS/devOps.js
+++ JS/devOps.js
@@ -8,26 +8,33 @@ Code help: https://www.w3schools.com/
 
 // Get the modal
 var modal01 = document.getElementById("videoModal01");
-
-
+var modal02 = document.getElementById("videoModal02");
 
 // Get the button that opens the modal
 var btn01 = document.getElementById("videoBtn01");
+var btn02 = document.getElementById("videoBtn02");
 
 
 
 // Get the <span> element that closes the modal
 var span01 = document.getElementsByClassName("close01")[0];
+var span02 = document.getElementsByClassName("close02")[0];
 
 
 // When the user clicks on the button, open the modal
 btn01.onclick = function() {
   modal01.style.display = "block";
 }
+btn02.onclick = function() {
+  modal02.style.display = "block";
+}
 
 // When the user clicks on <span> (x), close the modal
 span01.onclick = function() {
   modal01.style.display = "none";
+}
+span02.onclick = function() {
+  modal02.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -36,5 +43,10 @@ window.onclick = function(event) {
   if (event.target === modal01) {
     modal01.style.display = "none";
   }
-}
+
+  window.onclick = function(event) {
+    if (event.target === modal02) {
+      modal02.style.display = "none";
+    }
+  }}
 
