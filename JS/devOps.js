ca80283d17b414a1867c6954e2b2a163ccd0030f
Gabriel Sanchez <gjsanchez@my365.bellevue.edu>
2020-02-09 23:21:28 -0600

    week-6

--- JS/devOps.js
+++ JS/devOps.js
@@ -11,6 +11,7 @@ var modal01 = document.getElementById("videoModal01");
 var modal02 = document.getElementById("videoModal02");
 var modal03 = document.getElementById("videoModal03");
 var modal04 = document.getElementById("videoModal04");
+var modal05 = document.getElementById("videoModal05");
 
 
 // Get the button that opens the modal
@@ -18,6 +19,7 @@ var btn01 = document.getElementById("videoBtn01");
 var btn02 = document.getElementById("videoBtn02");
 var btn03 = document.getElementById("videoBtn03");
 var btn04 = document.getElementById("videoBtn04");
+var btn05 = document.getElementById("videoBtn05");
 
 
 
@@ -26,6 +28,7 @@ var span01 = document.getElementsByClassName("close01")[0];
 var span02 = document.getElementsByClassName("close02")[0];
 var span03 = document.getElementsByClassName("close03")[0];
 var span04 = document.getElementsByClassName("close04")[0];
+var span05 = document.getElementsByClassName("close05")[0];
 // When the user clicks on the button, open the modal
 btn01.onclick = function() {
   modal01.style.display = "block";
@@ -39,6 +42,9 @@ btn03.onclick = function() {
 btn04.onclick = function() {
   modal04.style.display = "block";
 }
+btn05.onclick = function() {
+  modal05.style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
 span01.onclick = function() {
   modal01.style.display = "none";
@@ -51,6 +57,9 @@ span03.onclick = function() {
 }
 span04.onclick = function() {
   modal04.style.display = "none";
+}
+span05.onclick = function() {
+  modal05.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
 
@@ -73,5 +82,10 @@ window.onclick = function(event) {
   .onclick = function(event) {
     if (event.target === modal04) {
       modal04.style.display = "none";
+    }
+  }
+  .onclick = function(event) {
+    if (event.target === modal05) {
+      modal05.style.display = "none";
     }
   }
